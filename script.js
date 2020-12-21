0cf3fa3c943b0cc333b836bc282d62fe60202388
shoneriki <ikirenohs@gmail.com>
2020-12-22 08:59:35 +0900

    javascript countdown

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 // current date
 // subtract to find days, minutes, seconds,
 
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
 const newYears = "1 Jan 2021";
 
 function countdown() {
@@ -9,14 +14,17 @@ function countdown() {
   const currentDate = new Date();
 
   // need to convert the milliseconds in console as days, hours, minutes, seconds
-  const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+  const totalSeconds =  (newYearsDate - currentDate) / 1000;
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours, minutes, seconds)
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
 }
 
 countdown();
