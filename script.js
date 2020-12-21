4ed04899d1ded60c7607bcbbbd834f83b4bbd07e
shoneriki <ikirenohs@gmail.com>
2020-12-22 08:47:23 +0900

    days-seconds working on console

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ function countdown() {
   const currentDate = new Date();
 
   // need to convert the milliseconds in console as days, hours, minutes, seconds
-  const seconds = new Date(newYearsDate - currentDate) / 1000;
+  const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(seconds / 3600 / 24);
-  const hours = Math.floor(seconds / 3600) %24;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours)
+  console.log(days, hours, minutes, seconds)
 }
 
 countdown();
